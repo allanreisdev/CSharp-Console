11bf26a6c825c7b273167dde8c01d81636b4d864
Allan Reis <allanreisdev@outlook.com>
2025-03-05 00:20:54 -0300

    Adicionar projeto e exemplo de operadores de comparação
    Criação do arquivo de projeto `ComparisonOperatorsCSharp.csproj` com configurações para .NET 8.0 e habilitação de usabilidades implícitas e verificação de nulidade.
    
    Adição do arquivo de solução `ComparisonOperatorsCSharp.sln` para gerenciamento no Visual Studio.
    
    Atualização do `Program.cs` com código que demonstra o uso de operadores de comparação, incluindo exemplos de comparação entre inteiros, strings e booleanos.

---
+++ ComparisonOperatorsCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Operadores de Comparação em C#");
+
+// Variáveis para comparação
+int numero1 = 10;
+int numero2 = 5;
+double numeroDecimal = 10.0;
+string texto1 = "C Sharp";
+string texto2 = "C Sharp";
+string texto3 = "Java";
+bool verdadeiro = true;
+bool falso = false;
+
+// Operador Igual a == 
+
+Console.WriteLine($"\nOperador Igual a == ");
+Console.WriteLine($"{numero1} == {numero2}: {numero1 == numero2}");
+Console.WriteLine($"{numero1} = 10: {numero1 == 10}");
+Console.WriteLine($"{texto1} == {texto2}: {texto1 == texto2}");
+Console.WriteLine($"{texto1} == {texto3}: {texto1 == texto3}");
+Console.WriteLine($"{verdadeiro} == {falso}: {verdadeiro == falso}");
+Console.WriteLine($"{verdadeiro} = true: {verdadeiro == true}");
+
