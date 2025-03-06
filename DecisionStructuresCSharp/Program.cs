65603c1511f20e43819be99243bce3b79bcfc2c3
Allan Reis <allanreisdev@outlook.com>
2025-03-06 02:06:43 -0300

    Adiciona projeto C# com exemplos de estruturas de decisão
    Foram criados os arquivos de projeto e solução para um aplicativo C# que utiliza estruturas de decisão. O projeto foi configurado para o .NET 8.0, com suporte a namespaces implícitos e verificação de nulidade. O arquivo `Program.cs` foi atualizado com exemplos práticos de `if`, `else` e `if-else if-else`, abordando verificações de idade, paridade de números, notas e condições climáticas.

---
+++ DecisionStructuresCSharp/Program.cs
@@ -0,0 +1,79 @@
+﻿Console.WriteLine($"Estrutura de Decisão em C# if,else");
+
+// Exemplo 1: if simples
+
+int idade = 25;
+
+if (idade >= 18)
+{
+    Console.WriteLine($"Voce é maior de idade");
+}
+
+Console.WriteLine($"Continuação do programa");
+
+// Exemplo 2: if else
+
+int numero = 7;
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"{numero} é um número par");
+}
+else
+{
+    Console.WriteLine($"{numero} é um número ímpar");
+}
+
+Console.WriteLine($"Continuação do programa");
+
+// Exemplo3: if else if else
+
+int nota = 85;
+string resposta;
+
+if (nota >= 90)
+{
+    resposta = "A";
+}
+else if (nota >= 80)
+{
+    resposta = "B";
+}
+else if (nota >= 60)
+{
+    resposta = "C";
+}
+else if (nota >= 40)
+{
+    resposta = "D";
+}
+else
+{
+    resposta = "Reprovado";
+}
+
+Console.WriteLine($"Nota: {nota}, resposta: {resposta}");
+
+// Exemplo 4 if dentro de outro if
+
+int temperatura = 28; 
+bool estaChovendo = true;
+
+if (temperatura > 25)
+{
+    Console.WriteLine($"Esta calor");
+    if (!estaChovendo)
+    {
+        Console.WriteLine($"E não está chovendo, dia bom para sair!");
+    }
+    else
+    {
+        Console.WriteLine($"Mas está chovendo");
+    }
+}
+else
+{
+    Console.WriteLine($"Temperatura amena ou fria");
+}
+
+Console.WriteLine($"Continuação do programa");
