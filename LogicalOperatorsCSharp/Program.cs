28aab7b81f9e30b09ba0505665bcbd0a999d39a7
Allan Reis <allanreisdev@outlook.com>
2025-03-11 01:37:40 -0300

    Adicionar projeto C# com operadores lógicos
    Criação do arquivo de projeto `LogicalOperatorsCSharp.csproj` com configurações para .NET 8.0 e habilitação de namespaces implícitos. Adição do arquivo de solução `LogicalOperatorsCSharp.sln` para gerenciamento do projeto e suas configurações de compilação. Inclusão do arquivo `Program.cs` com exemplos práticos de operadores lógicos AND (`&&`), OR (`||`), e NOT (`!`), além de condições complexas, demonstrando seu uso no console.

---
+++ LogicalOperatorsCSharp/Program.cs
@@ -0,0 +1,75 @@
+﻿Console.WriteLine($"Operadores Lógicos em c# (&&, ||, !)");
+
+Console.WriteLine($"Operador AND (&&)");
+
+int idade = 20;
+bool temCarteira = true;
+bool estaChovendo = false;
+
+if (idade >= 18 && temCarteira)
+{
+    Console.WriteLine($"Pode dirigir!");
+}
+else
+{
+    Console.WriteLine($"Não pode dirigir");
+}
+
+Console.WriteLine($"Operador OR (||)");
+
+string diaDaSemana = "domingo";
+
+if(diaDaSemana == "sabado" || diaDaSemana == "domingo")
+{
+    Console.WriteLine($"É fim de semana!");
+}
+else
+{
+    Console.WriteLine($"Dia de semana.");
+}
+
+Console.WriteLine($"Operador NOT (!)");
+
+bool usuarioLogado = false;
+
+if(!usuarioLogado)
+{
+    Console.WriteLine($"Usuário não está logado. Por favor, faça login");
+}
+else
+{
+    Console.WriteLine($"Bem-vindo, usuário logado!");
+}
+
+if(!estaChovendo)
+{
+    Console.WriteLine($"Não esta chovendo");
+}
+else
+{
+    Console.WriteLine($"Esta chovendo");
+}
+
+Console.WriteLine($"Condição Complexa");
+
+idade = 16;
+bool temAutorizacaoPais = true;
+bool diaEnsolarado = false;
+
+if ((idade >= 18 || temAutorizacaoPais) && !diaEnsolarado)
+{
+    Console.WriteLine($"Pode ir ao parque de diversões com ou sem autorização se maior de idade, mas nao em dia ensolarado");
+}
+else
+{
+    Console.WriteLine($"Não pode ir ao parque de diversões");
+}
+
+if ((idade >= 16 && temCarteira) || (!estaChovendo && diaDaSemana != "segunda-feira"))
+{
+    Console.WriteLine($"Condição complexa é VERDADEIRA");
+}
+else
+{
+    Console.WriteLine($"Condição complexa é FALSA");
+}
